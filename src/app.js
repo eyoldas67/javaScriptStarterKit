8dcb8f7c9f54751cc9586d3d12f14bb1e1a0d3a3
eyoldas67 <eyoldas67@gmail.com>
2021-11-10 11:59:15 +0300

    10.11 ders sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,13 @@
-console.log("Merhaba Kodlama.io")
+console.log("Merhaba Kodlma.io")
 
-//JS type safe değildir
-let dolarBugun = 9.30
+let dolarDun=9.20;
+let dolarBugun=9.40;
 
-let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
-
-console.log(dolarDun)
-
-const euroDun = 11.2
-//euroDun = 11
-
-console.log(euroDun)
-
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
+let konutKredileri = ["konut kredisi","emlak konut kredisi","Kamu Konut kredisi","ozel konut kredisi"]
+console.log(konutKredileri);
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i = 0; i < konutKredileri.length; i++) {
+   console.log("<li>"+konutKredileri[i]+"</li>")
+    
 }
 console.log("</ul>")
-
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
-
-console.log(konutKredileri)
